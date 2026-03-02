609b7a814e63c04c441a2bc2adab37d68a9384c0
Kamlesh Prajapati <kamleshprajapati0928@gmail.com>
2026-03-02 17:06:50 +0530

    Add boilerplate code

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Root route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
